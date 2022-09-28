aa7f81bdc24535efa11a7beb604a2a343b1ec42c
dg <dgeorge@capcenter.com>
2022-09-28 15:15:17 -0400

    Add SVGs

---
+++ src/assets/svg/ClipboardOutlineSVG.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const ClipboardOutlineSVG = () => {
+	return (
+		<svg width="24" height="30" viewBox="0 0 24 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clipPath="url(#clip0_412_3579)">
+			<path d="M20.9608 23.1084L15.8408 27.4327V23.1084H20.9608Z" fill="#FFE5D1"/>
+			<path d="M12.0009 0.405273C13.4153 0.405273 14.5609 1.37284 14.5609 2.56744H17.1209V5.27014H6.88086V2.56744H9.44086C9.44086 1.37284 10.5865 0.405273 12.0009 0.405273Z" fill="#FFE5D1"/>
+			<path d="M23.5205 4.72952V28.5133C23.5205 29.1079 22.9445 29.5944 22.2405 29.5944H1.76047C1.05647 29.5944 0.480469 29.1079 0.480469 28.5133V4.72952C0.480469 4.13492 1.05647 3.64844 1.76047 3.64844H3.04047V27.4322H15.8405L20.9605 23.1079V3.64844H22.2405C22.9445 3.64844 23.5205 4.13492 23.5205 4.72952Z" fill="white"/>
+			<path d="M13.9202 10.6759L10.0802 7.43262L6.24023 10.6759V14.4596H8.80023V11.7569H11.3602V14.4596H13.9202V10.6759Z" fill="white" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M17.1209 3.64852V2.56744H14.5609C14.5609 1.37284 13.4153 0.405273 12.0009 0.405273C10.5865 0.405273 9.44086 1.37284 9.44086 2.56744H6.88086V5.27014H17.1209V3.64852Z" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M19.0405 3.64844H22.2405C22.9445 3.64844 23.5205 4.13492 23.5205 4.72952V28.5133C23.5205 29.1079 22.9445 29.5944 22.2405 29.5944H1.76047C1.05647 29.5944 0.480469 29.1079 0.480469 28.5133V4.72952C0.480469 4.13492 1.05647 3.64844 1.76047 3.64844H4.96047" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M20.96 5.27051V23.1083L15.84 27.4327H3.04004V5.27051" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M5.60059 17.1621H8.16059" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M9.44043 17.1621H18.4004" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M5.60059 19.8643H8.16059" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M9.44043 19.8643H18.4004" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M5.60059 22.5674H8.16059" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M9.44043 22.5674H13.9204" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M15.8408 27.4327V23.1084H20.9608" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			</g>
+			<defs>
+				<clipPath id="clip0_412_3579">
+					<rect width="24" height="30" fill="white"/>
+				</clipPath>
+			</defs>
+		</svg>
+
+	)
+}
+
+export default ClipboardOutlineSVG;
---
+++ src/assets/svg/HomeWithRefreshSVG.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomeWithRefreshSVG = () => {
+	return (
+		<svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M16.4351 2.40527C16.4351 2.40527 16.446 2.41608 16.4677 2.43771C16.4894 2.41608 16.5002 2.40527 16.5002 2.40527L30.5937 16.4593L28.4254 18.6215L26.2572 16.4593L16.5002 6.7296L4.57496 18.6215L2.40674 16.4593L16.4351 2.40527Z" fill="#FFE5D1"/>
+			<path d="M20.327 27.7787C20.3053 27.6112 20.2944 27.4436 20.2944 27.2706C20.2944 24.8814 22.235 22.9463 24.6309 22.9463C26.2896 22.9463 27.7314 23.8814 28.4632 25.2436" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M28.9348 26.7617C28.9565 26.9293 28.9674 27.0969 28.9674 27.2698C28.9674 29.659 27.0268 31.5942 24.6309 31.5942C23.0318 31.5942 21.6333 30.7293 20.8799 29.4428" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M27.3413 28.3511L28.935 26.7619L28.9675 26.7295L30.5936 28.3511" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M21.9208 26.1895L20.3271 27.7786L20.2946 27.8111L18.6685 26.1895" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M16.4677 2.43771L16.5002 2.40527L30.5937 16.4593L28.4254 18.6215L26.2572 16.4593L16.5002 6.7296L4.57496 18.6215L2.40674 16.4593L16.4351 2.40527C16.4351 2.40527 16.446 2.41608 16.4677 2.43771V2.43771Z" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M23.0049 6.73005V4.02734H26.7993V10.5138" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M6.74316 18.6211V28.3509H13.2478V17.54H19.7525V23.486" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+			<path d="M26.2573 21.3238V16.459" stroke="#EA701B" strokeWidth="1.5" strokeLinejoin="round"/>
+		</svg>
+	)
+}
+
+export default HomeWithRefreshSVG;
